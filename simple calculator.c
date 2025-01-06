f9d173ff8407f7c83e4805e1ce1ee2a73f3845f5
Darun Nayeem <naimn2176@gmail.com>
2025-01-06 14:10:31 +0600

    Update simple calculator.c

--- simple calculator.c
+++ simple calculator.c
@@ -1,35 +1,87 @@
 #include <stdio.h>
+#include <math.h>
+
+double division(double, double);
+double modulus(int, int);
+void print_menu();
 
 int main() {
-    double number1, number2;
-    char operation;
-
-    printf("enter operation type(+,-,*,/) :\n");
-    scanf(" %c", &operation);
-
-    printf("enter two number :\n");
-    scanf("%lf %lf", &number1, &number2);
-
-    switch(operation) {
-        case '+':
-            printf("%.2lf%c%.2lf=%.2lf\n", number1, operation, number2, number1 + number2);
-            break;
-        case '-':
-            printf("%.2lf%c%.2lf=%.2lf\n", number1, operation, number2, number1 - number2);
-            break;
-        case '*':
-            printf("%.2lf%c%.2lf=%.2lf\n", number1, operation, number2, number1 * number2);
-            break;
-        case '/':
-            if (number2 == 0.0) {
-                printf("divide by zero situation\n");
-            } else {
-                printf("%.2lf%c%.2lf=%.2lf\n", number1, operation, number2, number1 / number2);
-            }
-            break;
-        default:
-            printf("invalid operation\n");
+  int choice;
+  double first, second, result;
+  while(1) {
+    print_menu();
+    scanf("%d", &choice);
+    if (choice == 7) {
+      break;
     }
 
-    return 0;
+    if (choice < 1 || choice > 7) {
+      fprintf(stderr, "Invalid Menu Choice.");
+      continue;
+    }
+
+    printf("\nPlease enter the first number: ");
+    scanf("%lf", &first);
+    printf("Now, enter the second number: ");
+    scanf("%lf", &second);
+
+    switch (choice)
+    {
+      case 1: // Add
+        result = first + second;
+        break;
+      case 2: // Subtract
+        result = first - second;
+        break;
+      case 3: // Multiply
+        result = first * second;
+        break;
+      case 4: // Divide
+        result = division(first, second);
+        break;
+      case 5: // Modulus
+        result = modulus(first, second);
+        break;
+      case 6: // Power
+        result = pow(first, second);
+        break;
+    }
+    if (!isnan(result)) {
+      printf("\nResult of operation is: %.2f", result);
+    }
+  };
+  
+  return 0;
+}
+
+double division(double a, double b) {
+  if (b == 0) {
+    fprintf(stderr, "Invalid Argument for Division");
+    return NAN;
+  } else {
+    return a / b;
+  }
+}
+
+double modulus(int a, int b) {
+  if (b == 0) {
+    fprintf(stderr, "Invalid Argument for Modulus");
+    return NAN;
+  } else {
+    return a % b;
+  }
+}
+
+void print_menu() {
+  printf("\n\n---------------------------------");
+  printf("\nWelcome to Simple Calculator\n");
+  printf("\nChoose one of the following options:");
+  printf("\n1. Add");
+  printf("\n2. Subtract");
+  printf("\n3. Multiply");
+  printf("\n4. Divide");
+  printf("\n5. Modulus");
+  printf("\n6. Power");
+  printf("\n7. Exit");
+  printf("\nNow, enter your choice: ");
 }
